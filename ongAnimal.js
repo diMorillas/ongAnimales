b75e5d5bfcdb4ce4c6a76f41cefc6365db0cf545
Didac Morillas <159442802+diMorillas@users.noreply.github.com>
2024-10-11 17:49:50 +0200

    First Commit

---
+++ ongAnimal.js
@@ -0,0 +1,64 @@
+window.onload = () => {
+    let animales = [
+        // Gatos
+        { nombre: "Wiskas", edad: 7, tipo: "gato", estadoAdopcion: false, imagen: "./img/wiskas.jpeg" },
+        { nombre: "Dave", edad: 1, tipo: "gato", estadoAdopcion: false, imagen: "./img/dave.jpeg" },
+        { nombre: "Artorias", edad: 2, tipo: "gato", estadoAdopcion: false, imagen: "./img/artorias.jpeg" },
+        { nombre: "Python", edad: 5, tipo: "gato", estadoAdopcion: false, imagen: "./img/python.webp" },
+
+        // Perros
+        { nombre: "Max", edad: 6, tipo: "perro", estadoAdopcion: false, imagen: "./img/max.jpeg" },
+        { nombre: "Luna", edad: 1, tipo: "perro", estadoAdopcion: false, imagen: "./img/luna.webp" },
+        { nombre: "Jie Li", edad: 20, tipo: "perro", estadoAdopcion: false, imagen: "./img/jieli.jpg" },
+        { nombre: "Luca", edad: 13, tipo: "perro", estadoAdopcion: false, imagen: "./img/luca.webp" },
+
+        // Pájaros
+        { nombre: "Kiriki", edad: 3, tipo: "ave", estadoAdopcion: false, imagen: "./img/kiriki.jpg" },
+        { nombre: "Chia", edad: 2, tipo: "ave", estadoAdopcion: false, imagen: "./img/chia.jpg" },
+    ];
+
+    const generaAnimales = () => {
+        let listaAnimal = document.getElementById('listaAnimal');
+
+        animales.forEach(animal => {
+            let card = document.createElement('div');
+            card.classList.add('card');
+
+            let imgAnimal = document.createElement('img');
+            imgAnimal.src = animal.imagen; 
+            imgAnimal.classList.add('card-img-top');
+            imgAnimal.alt = animal.nombre;
+
+            let cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+
+            let cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title', 'center');
+            cardTitle.textContent = animal.nombre;
+
+            let cardText = document.createElement('p');
+            cardText.classList.add('card-text', 'infoCarta', 'center');
+            if (animal.edad <= 1){
+                cardText.textContent = `¡Hola soy un ${animal.tipo} y tengo ${animal.edad} año!`; 
+            }else{
+                cardText.textContent = `¡Hola soy un ${animal.tipo} y tengo ${animal.edad} años!`;
+            }
+
+            let adoptButton = document.createElement('a');
+            adoptButton.href = "#containerPersonas";
+            adoptButton.classList.add('botonAdoptar');
+            adoptButton.textContent = "Adoptar";
+
+            
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+            cardBody.appendChild(adoptButton);
+            card.appendChild(imgAnimal);
+            card.appendChild(cardBody);
+            listaAnimal.appendChild(card);
+        });
+    }
+
+
+    generaAnimales(); 
+}
